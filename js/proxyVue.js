13a4a83452605f5eea698df0fdb777488ca9cede
lyc2014 <1006414710@qq.com>
2019-04-23 20:43:54 +0800

    proxy深度

--- js/proxyVue.js
+++ js/proxyVue.js
@@ -1,26 +1,31 @@
-function isObject(data) {
-  if(!data || typeof data !== 'object'){
-    return false
-  }
-  return true
-}
-function watch(data) {
-  if(!isObject) {
-    return data
-  }
-  Object.keys(data).forEach(function(key){
-    data[key] = watch(data[key])
-  })
-  return defineReactive(data)
-}
-function defineReactive(obj) {
-  return new Proxy(obj, {
-    get: (target, prop, receiver) => {
-      return Reflect.get(target, prop, receiver)
-    },
-    set: (target, prop, receiver) => {
-      console.log('set==>', prop, value)
-      return Reflect.set(target, prop, watch(value))
+function watch (obj) {
+    if (!isObject(obj)) {
+       return obj
     }
-  })
-}
+  
+    // 遍历所有的属性，进行Proxy处理
+    Object.keys(obj).forEach(key => {
+      obj[key] = watch(obj[key])
+    })
+   
+    return defineReactive(obj)
+  }
+  
+  function defineReactive(obj) {
+    return new Proxy(obj, {
+      get: (target, prop, receiver) => {
+        console.log('get')
+        return Reflect.get(target, prop, receiver)
+      },
+      set: (target, prop, value) => {    
+        // TODO触发监听
+        console.log('set==>', prop, value)
+        return Reflect.set(target, prop, watch(value))
+      }
+    })
+  }
+  
+  function isObject(obj) {
+    return Object(obj) === obj
+  }
+  
